567ead6ff4b651e475a4b21d20bf8238c9db5ed5
Flavio Macedo <fhenrique609@gmail.com>
2024-06-21 08:59:45 -0300

    First Commit

---
+++ weather/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import City
+
+class CityForm(forms.ModelForm):
+    class Meta:
+        model = City
+        fields = ['name']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+                'placeholder': 'City Name'
+            })
+        }
